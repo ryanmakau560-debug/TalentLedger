707c16902df8ba93d458e60932627d7a7452e3bb
Ryan <ryanmakau560@gmail.com>
2026-06-17 14:35:43 +0300

    feat: implement dashboard view, template, and url

--- talentledger/talentledger/urls.py
+++ talentledger/talentledger/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from ledger.views import profile_view
+from ledger.views import dashboard_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('dashboard/', dashboard_view, name='dashboard'),
     path('accounts/', include('django.contrib.auth.urls')), 
-   
     path('accounts/profile/', profile_view, name='profile'),
 ]
